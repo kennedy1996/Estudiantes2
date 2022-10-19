fd962cd6f76dd365ab105caaa4677287424e714b
kennedy1996 <87372917+kennedy1996@users.noreply.github.com>
2022-10-19 20:04:10 +0100

    Created Adapter
    - Created Adapter
    - Setter Adapter on Activity

--- app/src/main/java/com/estudiantes/ui/ListEstudiantesActivity.kt
+++ app/src/main/java/com/estudiantes/ui/ListEstudiantesActivity.kt
@@ -3,8 +3,11 @@ package com.estudiantes.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.estudiantes.R
 import com.estudiantes.inicializatorFirebase
+import com.estudiantes.ui.adapter.ListEstudiantesAdapter
 import com.estudiantes.ui.dialog.dialogNewEstudiante
 import com.estudiantes.ui.viewModel.EstudiantesViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -16,6 +19,8 @@ class ListEstudiantesActivity : AppCompatActivity() {
         provider.get(EstudiantesViewModel::class.java)
     }
 
+    private var adapter: RecyclerView.Adapter<ListEstudiantesAdapter.ViewHolder>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_estudiantes_activity)
@@ -25,8 +30,18 @@ class ListEstudiantesActivity : AppCompatActivity() {
         val fab = findViewById<FloatingActionButton>(R.id.list_estudiantes_activity_fab)
 
         fab.setOnClickListener {
-            dialogNewEstudiante(this, viewModel)
+//            dialogNewEstudiante(this, viewModel)
+            adapter!!.notifyDataSetChanged()
         }
+
+        val recyclerView: RecyclerView = findViewById(R.id.list_estudiantes_activity_recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        adapter = ListEstudiantesAdapter(
+            this,
+            viewModel
+        )
+        recyclerView.adapter = adapter
 
     }
 }
---
+++ app/src/main/java/com/estudiantes/ui/adapter/ListEstudiantesAdapter.kt
@@ -0,0 +1,51 @@
+package com.estudiantes.ui.adapter
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.estudiantes.R
+import com.estudiantes.ui.viewModel.EstudiantesViewModel
+
+class ListEstudiantesAdapter(
+    private val context: Context,
+    private var viewModel: EstudiantesViewModel
+) : RecyclerView.Adapter<ListEstudiantesAdapter.ViewHolder>(){
+
+    private val list = viewModel.getSearchEstudianteFirebase()
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nombre: TextView = itemView.findViewById(R.id.list_estudiantes_activity_item_nombre)
+        val edad: TextView = itemView.findViewById(R.id.list_estudiantes_activity_item_edad)
+        val ciudad: TextView = itemView.findViewById(R.id.list_estudiantes_activity_item_ciudad)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListEstudiantesAdapter.ViewHolder {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.list_estudiantes_activity_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        try {
+            var listAdapter = list?.value?.get(position)!!
+            holder.nombre.text = listAdapter.name
+            holder.ciudad.text = listAdapter.city
+            holder.edad.text= listAdapter.age.toString()
+
+        } catch (e: Exception) {
+            Log.e("ListEstudiantesAdapter", e.message.toString())
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list?.value?.count() ?: 0
+    }
+}
